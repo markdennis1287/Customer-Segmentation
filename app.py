6ac6b7dfbd1bf988579f9faf27005c2f2d419a2d
Dennis Miring'u <167270355+markdennis1287@users.noreply.github.com>
2025-05-02 11:16:25 +0000

    Redirect URI trial fix

--- app.py
+++ app.py
@@ -44,6 +44,7 @@ google = oauth.register(
         'scope': 'openid email profile',
         'prompt': 'select_account'
     }
+    redirect_uri='https://customer-segmentation-o8ml.onrender.com/google/callback'
 )
 
 @app.route('/')
